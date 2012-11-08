88010fed7157bbf930aaf0b963f70c1cb6ef0e24
Tom <tomasloon@gmail.com>
2012-11-07 23:49:13 -0300

    Widgets module started
    + Container setup
    + add, remove, parse methods started

--- app.js
+++ app.js
@@ -6,9 +6,42 @@
         $win = $(global),
         $doc = $(document),
         $body = $('body');
+
+    App.widgets = {
+        _container: null,
+        _list: {},
+        _count: 0,
+        add: function(obj) {
+            var key = 'widget-' + (this._count++);
+            this._list[key] = this.parse(obj);
+            this._list[key].appendTo(this._container);
+        },
+        remove: function() {
+
+        },
+        /**
+         * @param obj Contains information (array/object) and type (string) keys.
+         * @return jQuery div.widget
+         */
+        parse: function(obj) {
+            var widget = $('<div class="widget"></div>');
+            // do something!
+            // TODO: remove next line (it's for testing purposes only)
+            widget.text(obj.type);
+
+            return widget;
+        }
+    };
  
     App.init = function () {
         // something
+        App.widgets._container = $('.widgets-container');
+
+        App.widgets.add({
+            type: 'piechart',
+            information: {}
+        });
+
         $.getJSON('tweets.json', function(tweets){
             hhba.tweets = tweets;
             console.log(tweets);
